3754611024626b80c01b8859d54e22238badb599
Matheus Bruggemann Vieira <matheusbvieira@hotmail.com>
2021-05-29 01:31:37 -0300

    fix: lista serviços sem avaliação cadastrada

--- src/main/java/br/com/servicos/servicosApi/domain/repository/ServicoRepository.java
+++ src/main/java/br/com/servicos/servicosApi/domain/repository/ServicoRepository.java
@@ -11,7 +11,7 @@ import br.com.servicos.servicosApi.domain.model.Servico;
 public interface ServicoRepository extends CustomJpaRepository<Servico, Long> {
 	
 //	@Query(value = "select new Servico(avg(a.nota) as notaMedia, s.id as id,s.titulo as titulo,s.descricao as descricao, s.categoria as categoria, s.prestadorServico as prestadorServico ) from Avaliacao a join Servico s on s.categoria=:categoria")
-	@Query(value = "select new Servico(avg(a.nota) as notaMedia, s) from Avaliacao a join Servico s on s.categoria=:categoria")
+	@Query(value = "select new Servico(COALESCE(avg(a.nota),0) as notaMedia, s) from Avaliacao a right join Servico s on s.categoria=:categoria")
 	Page<Servico> findByCategoriaComNota(@Param("categoria")Categoria categoria, Pageable paginacao);
 	
 }
