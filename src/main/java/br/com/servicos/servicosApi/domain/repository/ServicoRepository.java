ed555d63257b118af8033dad741da3d65ac44797
Matheus Bruggemann Vieira <matheusbvieira@hotmail.com>
2021-06-01 11:21:14 -0300

    fix: altera query jpql para listagem de servicos com media.

--- src/main/java/br/com/servicos/servicosApi/domain/repository/ServicoRepository.java
+++ src/main/java/br/com/servicos/servicosApi/domain/repository/ServicoRepository.java
@@ -10,8 +10,7 @@ import br.com.servicos.servicosApi.domain.model.Servico;
 
 public interface ServicoRepository extends CustomJpaRepository<Servico, Long> {
 	
-//	@Query(value = "select new Servico(avg(a.nota) as notaMedia, s.id as id,s.titulo as titulo,s.descricao as descricao, s.categoria as categoria, s.prestadorServico as prestadorServico ) from Avaliacao a join Servico s on s.categoria=:categoria")
-	@Query(value = "select new Servico(COALESCE(avg(a.nota),0) as notaMedia, s) from Avaliacao a right join Servico s on s.categoria=:categoria")
+	@Query(value = "select new Servico(COALESCE(avg(a.nota),0) as notaMedia, s) from Servico s left join Avaliacao a on s.categoria=:categoria group by s.id")
 	Page<Servico> findByCategoriaComNota(@Param("categoria")Categoria categoria, Pageable paginacao);
 	
 }
